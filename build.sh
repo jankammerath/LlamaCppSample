e89e2532dfd855bdc02aaf69bfd7d63ceafd87f4
Jan Kammerath <jankammerath@gmail.com>
2025-02-12 15:47:27 +0100

    removed unnecessary commented code from build script

--- build.sh
+++ build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# cc -o llama_test main.cpp \
-#    -I/Users/jan/Development/llama.cpp/include \
-#    -I/Users/jan/Development/llama.cpp/ggml/include \
-#    -L/Users/jan/Development/llama.cpp/build/bin \
-#    -lllama -Wc++11-extensions -std=c++17 -fPIC -arch arm64
-
 cmake -B build
 cmake --build build --config Release
 cp ./build/llama_test ./llama_test 
